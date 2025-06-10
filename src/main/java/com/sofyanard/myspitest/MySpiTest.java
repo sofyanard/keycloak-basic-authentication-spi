1ca76db7bf2fabf355748c469a1623d477f760bf
Sofyan Ardianto <sofyanard@gmail.com>
2025-06-10 16:10:43 +0700

    trying more fun properties :blowfish:

--- src/main/java/com/sofyanard/myspitest/MySpiTest.java
+++ src/main/java/com/sofyanard/myspitest/MySpiTest.java
@@ -9,6 +9,7 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserCredentialModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.credential.PasswordUserCredentialModel;
+import org.keycloak.models.credential.OTPCredentialModel;
 import org.keycloak.models.utils.HmacOTP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +28,24 @@ public class MySpiTest  implements Authenticator {
         // generate QR
         final String totpSecret = HmacOTP.generateSecret(20);
         log.info("totpSecret = {}", totpSecret);
+
+        // get user
+        UserModel user = authenticationFlowContext.getUser();
+        log.info("user = {}", user.getUsername());
+
+        // get realm
+        RealmModel realm = authenticationFlowContext.getRealm();
+        log.info("realm = {}", realm.getId());
+
+        // get totp is configured or not
+        KeycloakSession keycloakSession = authenticationFlowContext.getSession();
+        if (keycloakSession != null) {
+            boolean isTotpConfigured = keycloakSession.userCredentialManager().isConfiguredFor(realm, user, OTPCredentialModel.TYPE);
+            log.info("isTotpConfigured = {}", isTotpConfigured);
+        }
+        else {
+            log.error("Keycloak session is null");
+        }
 
         authenticationFlowContext.success();;
     }
